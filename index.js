104f8838b5c5c3132b49f6b0647400c5d1098fe6
zhanghaiyang <403724532@qq.com>
2016-08-23 14:10:55 +0800

    cli

---
+++ index.js
@@ -0,0 +1,28 @@
+﻿import net from 'net';
+
+let count = 0;
+let users = {};
+var server  = net.createServer((conn) => {
+    
+    conn.setEncoding('utf-8');
+
+    conn.write('\n > people numbers: ' + count);
+
+    conn.write('\n > enter your username:')
+
+    count ++;
+
+
+    conn.on('close', () => {
+        count --;
+    });
+
+    conn.on('data', (data) => {
+        console.log(data);
+    });
+
+});
+
+server.listen(3000, function() {
+    console.log('server is run on *:3000 ');
+})
